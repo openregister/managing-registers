cb68020678ac62eb13c215e706304a05f556855c
Enda McCormack <endamccormack0@gmail.com>
2017-05-04 11:35:51 +0100

    update a register flow based on the actual register

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+
+  devise_for :users, controllers: {registrations: 'registrations'}
+
   resources :users, only: [:show]
 
   devise_scope :user do
@@ -7,12 +9,15 @@ Rails.application.routes.draw do
       root to: 'registers#index'
     end
     unauthenticated :user do
-      root to: 'devise/sessions#new', as: :unauthenticated_root
+      root to: 'devise/sessions#form', as: :unauthenticated_root
     end
   end
 
-  resources :register, except: [:destroy]
-  resources :registers, except: [:destroy]
-
   post 'select_register', to: "registers#select_register"
+
+  get '/:register', to: 'register#index'
+  get '/:register/:key/edit', to: 'register#edit'
+  get '/:register/new', to: 'register#new'
+  post '/:register', to: 'register#create'
+
 end
