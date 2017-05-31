2fffbaafc04da91dd135873308ee125cbc47877b
Dan Mitchell <dan@digitalreflow.co.uk>
2017-05-31 15:49:46 +0100

    Add password validation for min length
    The form builder enables us to add hint text as locales however this
    means that every password field on the user model will get the hint
    text but we don’t want it on the sign in form so its being hidden with
    CSS

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :teams, through: :team_members
 
   validates :full_name, presence: true
+  validates :password, length: { minimum: 8 }
 
   accepts_nested_attributes_for :team_members
 
