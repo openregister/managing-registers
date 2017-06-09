5799910b3748780c491dc7fd752b2941aaf8cdc2
Enda McCormack <endamccormack0@gmail.com>
2017-06-09 04:00:16 +0100

    added functionality for creating custodians

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,14 +70,33 @@ class UsersController::InvitationsController < Devise::InvitationsController
       u.skip_invitation = true
     end
 
-    # update team_members because the invite save only saves user
     user = User.find_by_email(resource_params[:email])
-    user.team_members.create(team_id: resource_params[:team_members_attributes]['0'][:team_id],
-                             role: resource_params[:team_members_attributes]['0'][:role]).save
+
+    if current_user.admin?
+      registers = Array.new
+
+      loop.with_index{|_, i|
+        register_name = resource_params[:teams_attributes][i.to_s][:registers]
+
+        unless register_name.nil?
+          registers.push(register_name)
+        end
+        break if resource_params[:teams_attributes][(i + 1).to_s].nil?
+      }
+
+      registers = registers.uniq
+      team = Team.new(registers: registers)
+      user.team_members.create(role: 'custodian', team: team).save
+    else
+      # update team_members because the invite save only saves user
+      user.team_members.create(team_id: resource_params[:team_members_attributes]['0'][:team_id],
+                               role: resource_params[:team_members_attributes]['0'][:role]).save
+    end
 
     # send email with injected params
     user.deliver_invitation
     user
+
   end
 
 end
