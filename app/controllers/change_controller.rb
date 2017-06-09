01dacc312e3346424092996da21bd6245f300ea0
Enda McCormack <endamccormack0@gmail.com>
2017-06-09 09:48:01 +0100

    finished off approve deny flow, functionality awaiting

--- app/controllers/change_controller.rb
+++ app/controllers/change_controller.rb
@@ -2,8 +2,8 @@ class ChangeController < ApplicationController
 
   include ApplicationHelper
 
-  def index
-    @change = Change.find(params['change_id'])
+  def show
+    @change = Change.find(params['id'])
     @register = get_register(@change.register_name)
     @new_register_record = @change.payload
     @current_register_record = OpenRegister.record(@change.register_name,
@@ -12,11 +12,30 @@ class ChangeController < ApplicationController
     if @current_register_record != nil
       @current_register_record = convert_register_json(@current_register_record)
     end
-
-    puts "hi"
   end
 
   def edit
+    puts "hello"
+    if params[:approve] == 'yes'
+      @change = Change.find(params['id'])
+      @register = get_register(@change.register_name)
+      @new_register_record = @change.payload
+      @current_register_record = OpenRegister.record(@change.register_name,
+                                                     @change.payload[@change.register_name],
+                                                     @register_phase)
+      if @current_register_record != nil
+        @current_register_record = convert_register_json(@current_register_record)
+      end
+    else
+      @change_user = Change.find(params['id']).user
+    end
+  end
+
+  def destroy
+
+  end
+
+  def update
 
   end
 
