ad7c3348f277d2ccf7b34e252d259b2d7bfd9440
Dan Mitchell <dan@digitalreflow.co.uk>
2017-06-19 11:54:01 +0100

    Move notify template ids into secrets

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -3,7 +3,7 @@ class DeviseMailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
 
   def reset_password_instructions(record, token, opts={})
-    set_template('b19e6e3a-0690-4102-a46e-e05f10552bd9')
+    set_template(Rails.application.secrets.notify_password_reset_mailer_template)
 
     set_personalisation(
       user_full_name: record.full_name,
@@ -14,7 +14,7 @@ class DeviseMailer < Devise::Mailer
   end
 
   def password_change(record, opts={})
-    set_template('7a4cd8d2-8a92-4dbd-84ed-9483ddf26dc4')
+    set_template(Rails.application.secrets.notify_password_change_mailer_template)
 
     set_personalisation(
       user_full_name: record.full_name
@@ -27,22 +27,22 @@ class DeviseMailer < Devise::Mailer
     case record.team_members.last.role
 
     when 'admin'
-      set_template('cca89657-03e4-4c88-9c9b-affd4c406ce6')
+      set_template(Rails.application.secrets.notify_admin_invite_template)
       set_personalisation(
         current_user_full_name: User.find(record.invited_by_id).full_name,
         invite_url: accept_invitation_url(record, invitation_token: token)
       )
 
     when 'custodian'
-      set_template('a59b23da-049c-49e4-b4dc-ed2ee05cb363')
+      set_template(Rails.application.secrets.notify_custodian_invite_template)
       set_standard_personalisations(record, token)
 
     when 'advanced'
-      set_template('9ada2d3b-744d-4d8e-9285-3af133d55bac')
+      set_template(Rails.application.secrets.notify_advanced_invite_template)
       set_standard_personalisations(record, token)
 
     when 'basic'
-      set_template('cda8bcc4-c814-4042-b191-ab57dcf7f7ea')
+      set_template(Rails.application.secrets.notify_basic_invite_template)
       set_standard_personalisations(record, token)
     end
 
