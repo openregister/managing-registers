b16ae522e208ec9926eaba2cab22204bb0ce8bf3
Dan Mitchell <dan@digitalreflow.co.uk>
2017-06-23 13:40:10 +0100

    Refactor admin user
    This change removes the team_members model from users defined as admin
    because they aren’t a team in the same was as other users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,10 @@ class UsersController < ApplicationController
   before_action :set_user
 
   def admin
-    @admin_users = User.joins(:team_members)
-                       .where(team_members: { role: 'admin' })
+    @admin_users = User.where(admin: true)
                        .where.not(invitation_accepted_at: nil)
 
-    @pending_users = User.joins(:team_members)
-                         .where(team_members: { role: 'admin' })
+    @pending_users = User.where(admin: true)
                          .where(invitation_accepted_at: nil)
 
     authorize! :admin, @users
@@ -46,7 +44,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:full_name, :email, :role, :password, :password_confirmation, :current_password, team_members_attributes: [:role])
+    params.require(:user).permit(:full_name, :email, :role, :admin, :password, :password_confirmation, :current_password, team_members_attributes: [:role])
   end
 
 end
@@ -63,29 +61,31 @@ class UsersController::InvitationsController < Devise::InvitationsController
 
     user = User.find_by_email(resource_params[:email])
 
-    if current_user.admin?
-      if params[:user][:role] == 'custodian'
-        registers = Array.new
+    unless user.admin?
+      if current_user.admin?
+        if params[:user][:role] == 'custodian'
+          registers = Array.new
 
-        loop.with_index{|_, i|
-          register_name = resource_params[:teams_attributes][i.to_s][:registers]
+          loop.with_index{|_, i|
+            register_name = resource_params[:teams_attributes][i.to_s][:registers]
 
-          unless register_name.nil?
-            registers.push(register_name)
-          end
-          break if resource_params[:teams_attributes][(i + 1).to_s].nil?
-        }
+            unless register_name.nil?
+              registers.push(register_name)
+            end
+            break if resource_params[:teams_attributes][(i + 1).to_s].nil?
+          }
 
-        registers = registers.uniq
-        team = Team.new(registers: registers)
-        user.team_members.create(role: params[:user][:role], team: team).save
+          registers = registers.uniq
+          team = Team.new(registers: registers)
+          user.team_members.create(role: params[:user][:role], team: team).save
+        else
+          user.team_members.create(role: params[:user][:role]).save
+        end
       else
-        user.team_members.create(role: params[:user][:role]).save
+        # update team_members because the invite save only saves user
+        user.team_members.create(team_id: resource_params[:team_members_attributes]['0'][:team_id],
+                                 role: resource_params[:team_members_attributes]['0'][:role]).save
       end
-    else
-      # update team_members because the invite save only saves user
-      user.team_members.create(team_id: resource_params[:team_members_attributes]['0'][:team_id],
-                               role: resource_params[:team_members_attributes]['0'][:role]).save
     end
 
     # send email with injected params
