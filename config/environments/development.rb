8c5b036edb5d121b303dccf982685db674d3328b
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-09-26 09:47:25 +0100

    move config to application.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,9 +5,6 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  config.autoload_paths += %W["#{config.root}/app/validators/"]
-
   # Do not eager load code on boot.
   config.eager_load = false
 
