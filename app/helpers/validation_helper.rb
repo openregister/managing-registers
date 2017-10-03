1817279f7c8a43be7fffeb82fdd1426d60035921
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-10-03 14:43:17 +0100

    move to own class and removed unused parameter

--- app/helpers/validation_helper.rb
+++ app/helpers/validation_helper.rb
@@ -1,22 +1,7 @@
 module ValidationHelper
 
-  class KeyUniquenessValidator < ActiveModel::Validator
-    def validate(record)
-      key = options[:register_name]
-      records = options[:records]
-      is_create = options[:is_create]
-      if ActiveModel::Type::Boolean.new.cast(is_create)
-        register_sym = key.underscore.to_sym
-        key_param = record.instance_variable_get("@#{key.underscore}")
-        if records.select {|r| r[:item][key] == key_param}.present?
-          record.errors.add register_sym, 'This record is already present'
-        end
-      end
-    end
-  end
-
   class DataValidator
-    def get_form_errors(params, field_definitions, register_name, action, records)
+    def get_form_errors(params, field_definitions, register_name, records)
 
       field_to_sym = ->(field){ field.underscore.to_sym }
 
@@ -29,13 +14,13 @@ module ValidationHelper
         end
         include ActiveModel::Validations
 
+        # Custom validators are defined in app/validators
         validates_with KeyUniquenessValidator, records: records, register_name: register_name, is_create: params[:is_create]
         field_definitions.each do |field|
           field_sym = field_to_sym.call(field[:item]['field'])
           attr_accessor field_sym
           is_key = field_sym == register_sym
           case field[:item]['datatype']
-            # Custom validators are defined in app/validators
           when 'integer'
             validates field_sym, numericality: { only_integer: true, message: "%{value} is not an integer" }, allow_blank: true
           when 'curie'
