fa194c89c9b192534f0b9900c7c2256ba890f1b8
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-10-03 16:25:09 +0100

    use error message suggested by UCD

--- app/validators/key_uniqueness_validator.rb
+++ app/validators/key_uniqueness_validator.rb
@@ -7,7 +7,7 @@ class KeyUniquenessValidator < ActiveModel::Validator
       register_sym = key.underscore.to_sym
       key_param = record.instance_variable_get("@#{key.underscore}")
       if records.select {|r| r[:item][key] == key_param}.present?
-        record.errors.add register_sym, 'Key must be unique'
+        record.errors.add register_sym, 'This code is already in use for another record, please use another code'
       end
     end
   end
