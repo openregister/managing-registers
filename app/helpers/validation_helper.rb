1f38e81558b4a424177cd6f0556611d9dec473e5
fjavierm <javier.marquez@digital.cabinet-office.gov.uk>
2017-10-10 15:39:23 +0100

    Validate that a linked record is a valid register record

--- app/helpers/validation_helper.rb
+++ app/helpers/validation_helper.rb
@@ -1,13 +1,11 @@
 module ValidationHelper
-
   class DataValidator
     def get_form_errors(params, field_definitions, register_name, records)
-
-      field_to_sym = ->(field){ field.underscore.to_sym }
+      field_to_sym = ->(field) { field.underscore.to_sym }
 
       register_sym = field_to_sym.call(register_name)
 
-      register_model_class = Class.new() do
+      register_model_class = Class.new do
         # This is required see: https://stackoverflow.com/a/14431888/238230
         def self.model_name
           ActiveModel::Name.new(self, nil, 'record')
@@ -22,11 +20,11 @@ module ValidationHelper
           is_key = field_sym == register_sym
           case field[:item]['datatype']
           when 'integer'
-            validates field_sym, numericality: { only_integer: true, message: "%{value} is not an integer" }, allow_blank: true
+            validates field_sym, numericality: { only_integer: true, message: '%{value} is not an integer' }, allow_blank: true
           when 'curie'
-            validates field_sym, format:{ with: /\A\w+:\w+\z/, message: "%{value} is not a valid curie"}, allow_blank: true
+            validates field_sym, linked: { register_linked: field[:item]['register'] }, allow_blank: true
           when 'string'
-            validates field_sym, presence: { message: "Field %{attribute} is required"}, allow_blank: !is_key
+            validates field_sym, presence: { message: 'Field %{attribute} is required' }, allow_blank: !is_key
           when 'url'
             validates field_sym, url: true, allow_blank: true
           when 'datetime'
@@ -35,7 +33,7 @@ module ValidationHelper
         end
       end
 
-      field_definitions_map = field_definitions.map{ |f| field_to_sym.call(f[:item]['field'])}
+      field_definitions_map = field_definitions.map { |f| field_to_sym.call(f[:item]['field']) }
       record = register_model_class.new
       field_definitions_map.each do |f|
         value = params[f.to_s.dasherize]
--- spec/helpers/validation_helper_spec.rb
+++ spec/helpers/validation_helper_spec.rb
@@ -6,21 +6,51 @@ RSpec.describe ValidationHelper do
   field_definitions = YAML.load_file('./spec/support/field_definitions.yaml')
   data_validator = ValidationHelper::DataValidator.new
 
-  describe "get_form_errors" do
-    it "returns an error if date is invalid" do
-      params = {"start-date"=>"foo", "country" => "zz"}
-      expect(data_validator.get_form_errors(params,field_definitions, 'country',  nil).messages).to eql({:start_date=>["Enter a valid date"]})
+  describe 'get_form_errors' do
+    before do
+      data = File.open('./spec/support/country_gm.tsv') { |f| f.read }
+      stub_request(:get, 'https://country.beta.openregister.org/record/GM.tsv')
+        .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip, deflate', 'Host' => 'country.beta.openregister.org' })
+        .to_return(status: 200, body: data, headers: {})
+
+      stub_request(:get, 'https://country.beta.openregister.org/record/ZZ.tsv')
+        .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip, deflate', 'Host' => 'country.beta.openregister.org' })
+        .to_return(status: 404)
     end
 
-    it "returns no errors if date is valid" do
-      params = {"start-date"=>"2015-05", "country"=>"xx" }
+    it 'returns an error if date is invalid' do
+      params = { 'start-date' => 'foo', 'country' => 'zz' }
+      expect(data_validator.get_form_errors(params, field_definitions, 'country', nil).messages).to eql(start_date: ['Enter a valid date'])
+    end
+
+    it 'returns no errors if date is valid' do
+      params = { 'start-date' => '2015-05', 'country' => 'xx' }
       expect(data_validator.get_form_errors(params, field_definitions, 'country', nil).details).to be_empty
     end
 
-    it "returns an error if key is not populated" do
-      params = {:country=>'', :register=>"country"}
-      expect(data_validator.get_form_errors(params, field_definitions, 'country',  nil).messages).to eql({:country => ["Field Country is required"]})
+    it 'returns an error if key is not populated' do
+      params = { country: '', register: 'country' }
+      expect(data_validator.get_form_errors(params, field_definitions, 'country', nil).messages).to eql(country: ['Field Country is required'])
+    end
+
+    it 'returns no errors if curie is valid' do
+      params = { 'curie-field' => 'country:GM', 'country' => 'aa' }
+      expect(data_validator.get_form_errors(params, field_definitions, 'country', nil).details).to be_empty
+    end
+
+    it 'returns an error if key does not exist' do
+      params = { 'curie-field' => 'country:ZZ', 'country' => 'aa' }
+      expect(data_validator.get_form_errors(params, field_definitions, 'country', nil).details).to eql(curie_field: [error: 'Must be valid data from a register that is ready to use'])
+    end
+
+    it 'returns an error if key is not in a valid format' do
+      params = { 'curie-field' => 'country::', 'country' => 'aa' }
+      expect(data_validator.get_form_errors(params, field_definitions, 'country', nil).details).to eql(curie_field: [error: 'Must be valid data from a register that is ready to use'])
+    end
+
+    it 'returns no errors if curie is empty' do
+      params = { 'curie-field' => '', 'country' => 'aa' }
+      expect(data_validator.get_form_errors(params, field_definitions, 'country', nil).details).to be_empty
     end
   end
-
 end
