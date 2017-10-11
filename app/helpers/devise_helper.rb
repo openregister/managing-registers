d76f6b2b6826be0e47997500357b264f892cf1c8
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-10-11 15:55:16 +0100

    customise Devise template to conform to GDS error style (#43)
    * add custom devise_helper in GDS style
    
    * move error messages above H1
    
    * remove example from ID

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,26 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.map { |k, v| content_tag(:li, link_to("#{k.to_s.humanize} #{v}", "##{resource.class.model_name.to_s.downcase}_#{k}")) }.join
+    headline = "Please correct the #{'error'.pluralize(resource.errors.count)} below"
+
+    html = <<-HTML
+
+  <div class="error-summary" role="alert" aria-labelledby="error-summary-heading" tabindex="-1">
+
+        <h2 class="heading-medium error-summary-heading" id="error-summary-heading">
+          #{headline}
+        </h2>
+
+
+        <ul class="error-summary-list">
+          #{messages}
+        </ul>
+
+      </div>
+    HTML
+
+    html.html_safe
+  end
+end
