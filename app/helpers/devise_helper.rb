17395501cbc4e34bd3645f69771f2897efaba826
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-10-12 13:27:43 +0100

    lower case error message attributes

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,7 +2,7 @@ module DeviseHelper
   def devise_error_messages!
     return "" if resource.errors.empty?
 
-    messages = resource.errors.map { |k, v| content_tag(:li, link_to("Your #{k.to_s.humanize} #{v}", "##{resource.class.model_name.to_s.downcase}_#{k}")) }.join
+    messages = resource.errors.map { |k, v| content_tag(:li, link_to("Your #{k.to_s.humanize(capitalize: false)} #{v}", "##{resource.class.model_name.to_s.downcase}_#{k}")) }.join
     headline = "Please correct the #{'error'.pluralize(resource.errors.count)} below"
 
     html = <<-HTML
