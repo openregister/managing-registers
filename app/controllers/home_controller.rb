18d047adc4d51b7bae7318a858928590479402c6
Dan Mitchell <dan@digitalreflow.co.uk>
2017-10-18 15:10:10 +0100

    Remove sorting of registers on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   include ApplicationHelper
 
   def index
-    @registers = OpenRegister.register('register', Rails.configuration.register_phase)._all_records.sort_by(&:key)
+    @registers = OpenRegister.register('register', Rails.configuration.register_phase)._all_records
   end
 end
