209e4be1a647efd89388fcdecd1fc7e630006d82
fjavierm <javier.marquez@digital.cabinet-office.gov.uk>
2017-11-21 11:55:07 +0000

    Add logs to the permission layer

--- app/permissions/user_policy.rb
+++ app/permissions/user_policy.rb
@@ -1,51 +1,92 @@
 class UserPolicy < Policy
   class << self
     def show?(current_user, user)
-      return false unless values_present? current_user, user
-
-      current_user.id == user.id
-    end
-
-    def custodians?(current_user)
-      permission?(current_user)
-    end
-
-    def new?(current_user, role, team_id)
-      return false unless values_present? current_user
-
-      return role.present? if current_user.admin?
-
-      if team_id.present?
-        unless member?(current_user, team_id)
-          return false
-        end
-      else
+      unless values_present? current_user, user
+        log "UserPolicy::#{__method__}: Not enough values to check."
         return false
       end
 
-      Responsibility.manager?(current_user)
+      if current_user.id == user.id
+        true
+      else
+        log "UserPolicy::#{__method__}: Users does not match #{current_user.id} != #{user.id}"
+        false
+      end
+    end
+
+    def custodians?(current_user)
+      permission?(current_user, 'custodians?')
+    end
+
+    def new?(current_user, role, team_id)
+      unless values_present? current_user
+        log "UserPolicy::#{__method__}: Not enough values to check."
+        return false
+      end
+
+      if current_user.admin?
+        if role.present?
+          return true
+        else
+          log "UserPolicy::#{__method__}: Role not present."
+          return false
+        end
+      end
+
+      if team_id.present?
+        unless member?(current_user, team_id)
+          log "UserPolicy::#{__method__}: The user #{current_user.id} is not a member of the team #{team_id}."
+          return false
+        end
+      else
+        log 'UserPolicy::new?: Team ID not present.'
+        return false
+      end
+
+      if Responsibility.manager?(current_user)
+        true
+      else
+        log "UserPolicy::#{__method__}: The user #{current_user} cannot manage the team #{team_id}."
+        false
+      end
     end
 
     def create?(current_user)
-      return false unless values_present? current_user
+      unless values_present? current_user
+        log "UserPolicy::#{__method__}: Not enough values to check."
+        return false
+      end
 
-      current_user.admin? || Responsibility.manager?(current_user)
+      if current_user.admin? || Responsibility.manager?(current_user)
+        true
+      else
+        log "UserPolicy::#{__method__}: The user #{current_user} does not have permissions."
+        false
+      end
     end
 
     def admin?(current_user)
-      permission?(current_user)
+      permission?(current_user, 'admin?')
     end
 
     def destroy?(current_user)
-      permission?(current_user)
+      permission?(current_user, 'destroy?')
     end
 
   private
 
-    def permission?(current_user)
-      return false unless values_present? current_user
+    def permission?(current_user, method_name)
+      unless values_present? current_user
+        log "UserPolicy::#{method_name}: Not enough values to check."
+        return false
+      end
 
-      current_user.admin?
+      if current_user.admin?
+        true
+      else
+        log "UserPolicy::#{method_name}: Not enough permission."
+        false
+      end
     end
 
     def member?(current_user, team_id)
