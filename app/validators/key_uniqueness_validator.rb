c21bb825bb89f094a312837095e57edb2d5821fd
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-23 12:14:53 +0100

    fix key uniqueness check

--- app/validators/key_uniqueness_validator.rb
+++ app/validators/key_uniqueness_validator.rb
@@ -2,9 +2,8 @@ class KeyUniquenessValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     records = options[:records]
     is_create = options[:is_create]
-    key = attribute.to_s.dasherize
     if ActiveModel::Type::Boolean.new.cast(is_create)
-      if records.select { |r| r.item.value[key] == value }.present?
+      if records.any? { |r| r.entry.key == value }
         record.errors.add attribute, 'This code is already in use for another record, please use another code'
       end
     end
